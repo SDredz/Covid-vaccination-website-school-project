1e8bcf85eb28ff2ea86036c51660bfe9e1a15a26
SDredz <87680346+SDredz@users.noreply.github.com>
2021-09-19 11:53:41 -0500

    First commit

---
+++ form.js
@@ -0,0 +1,10 @@
+function unhideDate() {
+    document.getElementById("date_name").style.display = "block";
+    document.getElementById("covid date").style.display = "block";
+}
+
+function hideDate() {
+    document.getElementById("date_name").style.display = "none";
+    document.getElementById("covid date").style.display = "none";
+
+}
